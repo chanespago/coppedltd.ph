0ca4749fc795d7329f1c7fb3e23d5871db26b1d3
chanespago <76593408+chanespago@users.noreply.github.com>
2021-08-05 13:52:09 +0800

    updates__Loader

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -43,3 +43,13 @@ window.onscroll = () => {
     const nav = document.getElementById('header')
     if (this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
+
+window.onload = () => {
+    const load = document.getElementById('loader-wrapper');
+    const bodyContent = document.getElementById('body-content');
+
+    setTimeout(function() {
+        load.style.display = "none";
+        bodyContent.style.display = "block";
+      }, 2000);
+}
